cecc37710d30a26e20d9a00a8caa56b2b1c2186c
= <=>
2024-03-05 22:13:56 +0100

    'First-version'

---
+++ CS/IpAdress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class IpAdress
+    {
+        public string GetIp()
+    {
+        var n = Dns.GetHostName();
+        var ip = Dns.GetHostByName(n).AddressList[0].ToString();
+        return ip;
+    }
+    
+    }
+
