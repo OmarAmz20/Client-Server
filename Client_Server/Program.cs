5a7d2b579dd244e59fe8a44ab78e6a153809e1c6
= <=>
2024-03-05 21:54:30 +0100

    'First-version'

---
+++ Client_Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cl = new CS.Client();
+            cl.Active();
+        }
+    }
+}
